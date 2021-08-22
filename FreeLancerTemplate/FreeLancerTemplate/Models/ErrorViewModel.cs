1ef8333785ca6b1bfbb462c4cab38ddd7a1a6cdc
HuseyinZorbeyZengin <huseyinzorbey34@gmail.com>
2021-08-22 22:51:11 +0300

    Project Files

---
+++ FreeLancerTemplate/FreeLancerTemplate/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FreeLancerTemplate.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
